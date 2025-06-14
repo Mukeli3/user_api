fe1b796e972942368a0048ac1203f56c73ed2ccc
Mukeli3 <mukelicynthia20@gmail.com>
2025-06-14 05:46:50 +0000

    fix: updated app to include a relational db

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
+import os
+import pymysql
 from flask import Flask
-from app.errors import handle_bad_request, handle_not_found
-from app.routes import add_user, get_single_user, list_users
-from app.routes import modify_user, delete_user
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 
+
+pymysql.install_as_MySQLdb()
+load_dotenv()
+db = SQLAlchemy()
+jwt = JWTManager()
 
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_object('app.config.Config')
+    db.init_app(app)
+    jwt.init_app(app)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config['JWT_TOKEN_LOCATION'] = ['headers']
+
+    migrate = Migrate(app, db)
+
     from app.routes import user
+
     app.register_blueprint(user)
+
+    with app.app_context():
+        from app.models import User
+        db.create_all()
 
     return app
